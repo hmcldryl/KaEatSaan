d1f37750c0b427fb5a5df0d5f62746daad658114
John Daryl Homecillo <daryl.homecillo@gmail.com>
2025-02-11 12:35:48 +0800

    chore: slight refactoring of namespaces

--- ka_eat_saan/lib/main.dart
+++ ka_eat_saan/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:ka_eat_saan/features/restaurant_picker/screens/home_page.dart';
+import 'package:ka_eat_saan/features/home/screens/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const KaEatSaanApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class KaEatSaanApp extends StatelessWidget {
+  const KaEatSaanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
